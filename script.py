53d0101434d175bba7a968bda1a8a40c834986bd
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-04-14 09:51:29 -0300

    Was with a little problem with ssl than find a solution in stack overflow by importing ssl and creating a default

--- script.py
+++ script.py
@@ -1,4 +1,8 @@
 from pytube import YouTube
 from sys import argv
+import ssl
+ssl._create_default_https_context = ssl._create_stdlib_context
 
-link = argv[1]
+yt = YouTube("https://www.youtube.com/watch?v=vEQ8CXFWLZUnk")
+
+print(yt.thumbnail_url)
