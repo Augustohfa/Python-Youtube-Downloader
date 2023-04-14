385674d1a322f509542fc8f1c04a8434d6d165a8
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-04-14 10:30:49 -0300

    Now working with MP4 format and getting the highest quality of the video, downloading direct to the desired folder in the computer

--- script.py
+++ script.py
@@ -3,24 +3,23 @@ from pytube import Playlist
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
-get_video_url = input('Type video URL: ')
+link = input('Type video URL: ')
 
-yt = YouTube(get_video_url)
+yt = YouTube(link).streams.filter(file_extension='mp4').get_highest_resolution(
+).download('/Users/macbook/Documents/DownloadedVideos')
 
-print(yt.thumbnail_url)  # Get thubmail URL
 
-yt.streams.first().download()
 # Creating a playlist with PyTube
 
-get_yt_playlist = input('Type the playlist URL: ')
-yt_playlist = Playlist(get_yt_playlist)
+# get_yt_playlist = input('Type the playlist URL: ')
+# yt_playlist = Playlist(get_yt_playlist)
 
-for video in yt_playlist.videos:
-    video.streams.first().download
+# for video in yt_playlist.videos:
+#     video.streams.first().download
 
-# Can also just print the URL's
+# # Can also just print the URL's
 
-for url in yt_playlist.video_urls:
-    print(url)
+# for url in yt_playlist.video_urls:
+#     print(url)
 
 # Currently working
