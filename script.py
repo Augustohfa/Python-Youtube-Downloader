bad2f8988ae267651713cffa87acd4704b4cdb61
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-04-14 10:11:18 -0300

    Already working with just a few lines, now reading the documentation to polish the code

--- script.py
+++ script.py
@@ -1,8 +1,26 @@
 from pytube import YouTube
-from sys import argv
+from pytube import Playlist
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
-yt = YouTube("https://www.youtube.com/watch?v=vEQ8CXFWLZUnk")
+get_video_url = input('Type video URL: ')
 
-print(yt.thumbnail_url)
+yt = YouTube(get_video_url)
+
+print(yt.thumbnail_url)  # Get thubmail URL
+
+yt.streams.first().download()
+# Creating a playlist with PyTube
+
+get_yt_playlist = input('Type the playlist URL: ')
+yt_playlist = Playlist(get_yt_playlist)
+
+for video in yt_playlist.videos:
+    video.streams.first().download
+
+# Can also just print the URL's
+
+for url in yt_playlist.video_urls:
+    print(url)
+
+# Currently working
